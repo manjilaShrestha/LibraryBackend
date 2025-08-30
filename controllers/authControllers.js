a456c9f261ef75fefa87345cf4987b58576e44d3
manjilaShrestha <manjila2005shrestha@gmail.com>
2025-08-30 22:50:02 +0545

    Message

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 import jwt from 'jsonwebtoken';
-import User from '../models/user';
+import User from '../models/user.js';
 
 // Register
 export const register = async (req, res) => {
