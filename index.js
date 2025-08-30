4131dd21825538586e86fcb99ccad6f63fb08f9c
manjilaShrestha <manjila2005shrestha@gmail.com>
2025-08-31 00:07:36 +0545

    Message

--- index.js
+++ index.js
@@ -23,8 +23,19 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL,
+//   credentials: true
+// }));
+
+const allowedOrigins = [
+  process.env.FRONTEND_URL,  
+  "http://localhost:5173"    
+];
+
+
 // Middleware
-app.use(cors({ origin: "*", credentials: true }));
+// app.use(cors({ origin: "*", credentials: true }));
 app.use(express.json());
 app.use(cookieParser());
 
@@ -68,7 +79,9 @@ mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => console.log(` Server running at http://localhost:${PORT}`));
+    app.listen(PORT, () =>  console.log(`Server running on port ${PORT}`) 
+  );
+  // console.log(` Server running at http://localhost:${PORT}`));
     seedLibrarian();
   })
   .catch((err) => {
